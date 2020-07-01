e34fb597426552e4ea3cf9907568570a202d73d8
Ayush Rai <ayushrai3096@gmail.com>
2020-07-01 14:21:08 +0530

    final patch

--- Controllers/Get_Sentiment_Controller/postGetSentiment.js
+++ Controllers/Get_Sentiment_Controller/postGetSentiment.js
@@ -3,25 +3,30 @@ const { response } = require("express");
 const sentimentService = 'https://salty-retreat-84216.herokuapp.com/api/ml';
 
 exports.postGetSentiment = (req, res, next) => {
-    const body = req.body.review;
-    console.log(body);
+    
+    const resBody = req.body.review;
 
     request.post({
-        url: `${sentimentService}/getKeywords`, form: { "sentence": `${body}` }
+        url: `${sentimentService}/getKeywords/`, form: { "sentence": `${resBody}` }
     }, function responseFunc(err, response, body) {
         if (err) {
             res.status(400).send({ problem: `Sentiment Service responded with issue ${err}` });
         }
+       
+        const transformedBody=JSON.parse(body);
+        let extractedKeywords = transformedBody.keywords;
 
         request.post({
-            url: `${sentimentService}/getSentiment`, form: { "sentence": `${body}` }
+            url: `${sentimentService}/getSentiment/`, form: { "sentence": `${resBody}` }
         },
             function responseFunc(err, response, body) {
                 if (err) {
                     res.status(400).send({ problem: `Sentiment Service responded with issue ${err}` });
                 }
-                console.log(body);
-                res.status(202).send(body);
+                res.status(202).send({
+                    keywords: extractedKeywords,
+                    sentiment: body
+                });
             })
 
     });
--- Controllers/Homepage_Controller/getHomepage.js
+++ Controllers/Homepage_Controller/getHomepage.js
@@ -1,4 +1,4 @@
 exports.getHomepage = (req, res, next) => {
-    res.send("<h1> hwllo </h1>");
+    res.send("<h1> Get request are not required here. This is onlyfor receiving post requests which will be sent to ML service </h1>");
 
 };
